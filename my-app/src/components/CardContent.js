e4c0758d2ab8328c73b821ee2b5e607dc08fc083
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-07-26 22:18:59 +0300

    add

--- my-app/src/components/CardContent.js
+++ my-app/src/components/CardContent.js
@@ -1,36 +1,42 @@
 // import "./CardContent.scss"
 // import CardContentJson from '../data/CardContent.json';
 
-export const CardContent = ({username, tag, location, avatar, stats }) => {
+export const CardContent = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats, }) => {
+  
     return (
         <div className="card-content-container">
-                <div class="profile">
-  <div class="description">
-    <img
-      src={avatar}
-      alt="User avatar"
-      className="avatar"
-    />
-    <p className="name">{username}</p>
-    <p className="tag">{tag}</p>
-    <p className="location">{location}</p>
-  </div>
+          <div className="profile">
+            <div className="description">
+              <img
+                src={avatar}
+                alt="User avatar"
+                className="avatar"
+              />
+                <p className="name">{username}</p>
+                <p className="tag">{tag}</p>
+                <p className="location">{location}</p>
+              </div>
 
-  <ul class="stats">
-    <li>
-      <span class="label">Followers</span>
-      <span class="quantity">1000</span>
-    </li>
-    <li>
-      <span class="label">Views</span>
-      <span class="quantity">2000</span>
-    </li>
-    <li>
-      <span class="label">Likes</span>
-      <span class="quantity">3000</span>
-    </li>
-  </ul>
-</div>
+              <ul class="stats">
+                <li>
+                  <span className="label">Followers</span>
+                  <span className="quantity">{stats.followers}</span>
+                </li>
+                <li>
+                  <span className="label">Views</span>
+                  <span className="quantity">{stats.views}</span>
+                </li>
+                <li>
+                  <span className="label">Likes</span>
+                  <span className="quantity">{stats.likes}</span>
+                </li>
+              </ul>
+          </div>
         </div>
     )
 }
